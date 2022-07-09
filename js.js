e7380a117d3e0d2f6696a7b410e960bf4657eb4d
LevonGhaziyan <104306155+LevonGhaziyan@users.noreply.github.com>
2022-07-10 00:22:37 +0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,31 @@
+lan = "am"
+function lan_am() {
+    lezu = "am"
+    document.getElementById("title").innerHTML = "Լևոն Ղազիյան"
+    document.getElementById("title1").innerHTML = "Սիրում եմ զբաղվել սպորտով, ծրագրավորմամբ և մաթեմատիկայով:"
+    document.getElementById("sw").innerHTML = "Իմ սոցիալական ցանցերը"
+    document.getElementById("hobbies").innerHTML = "Իմ հոբիները"
+    document.getElementById("hobby1").innerHTML = "Ծրագրավորում"
+    document.getElementById("hobby2").innerHTML = "Սպորտ՝ ֆուտբոլ, շախմատ"
+    document.getElementById("works").innerHTML = "Իմ աշխատանքները"
+}
+function lan_ru() {
+    lezu = "ru"
+    document.getElementById("title").innerHTML = "Левон Газиян"
+    document.getElementById("title1").innerHTML = "Люблю заниматься спортом, программированием и математикой."
+    document.getElementById("sw").innerHTML = "Мои социальные сети"
+    document.getElementById("hobbies").innerHTML = "Мои хобби"
+    document.getElementById("hobby1").innerHTML = "Программирование(с 2019-ого года)"
+    document.getElementById("hobby2").innerHTML = "Спорт: футбол, шахматы"
+    document.getElementById("works").innerHTML = "Мои работы"
+}
+function lan_en() {
+    lezu = "en"
+    document.getElementById("title").innerHTML = "Levon Ghaziyan"
+    document.getElementById("title1").innerHTML = "I'm into sports, programming and maths. "
+    document.getElementById("sw").innerHTML = "My social networks"
+    document.getElementById("hobbies").innerHTML = "My hobbies"
+    document.getElementById("hobby1").innerHTML = "Programming (since 2019)"
+    document.getElementById("hobby2").innerHTML = "Sports: football, chess"
+    document.getElementById("works").innerHTML = "My works"
+}
